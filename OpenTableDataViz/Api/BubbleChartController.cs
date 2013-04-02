3b58ca54f80928f94900ea200fb9341f0cde6c90
Govin <githubgovin@gmail.com>
2013-04-01 18:01:17 -0700

    bubble chart changes

--- OpenTableDataViz/Api/BubbleChartController.cs
+++ OpenTableDataViz/Api/BubbleChartController.cs
@@ -5,52 +5,52 @@ using System.Web;
 
 namespace OpenTableDataViz.Api
 {
-	using System.Net.Http;
-	using System.Web.Http;
+    using System.Net.Http;
+    using System.Web.Http;
 
-	using OpenTableDataViz.Services;
+    using OpenTableDataViz.Services;
 
-	public class BubbleChartController : BaseApiController
-	{
-		private readonly IBusinessQuery query;
+    public class BubbleChartController : BaseApiController
+    {
+        private readonly IBusinessQuery query;
 
-		private readonly IAppConfiguration config;
+        private readonly IAppConfiguration config;
 
-		public BubbleChartController()
-		{
-		}
+        public BubbleChartController()
+        {
+        }
 
-		public BubbleChartController(IBusinessQuery query, IAppConfiguration config)
-		{
-			this.query = query;
-			this.config = config;
-		}
+        public BubbleChartController(IBusinessQuery query, IAppConfiguration config)
+        {
+            this.query = query;
+            this.config = config;
+        }
 
-		[HttpGet]
-		public HttpResponseMessage Get(string region)
-		{
-				switch (region.ToLower().Trim())
-				{
-					case "eu":
-						return this.GetResponse(this.query.GetResoCountBubbleChartData(15, this.config.ResoFeedUrlEU));
-					case "asia":
-						return this.GetResponse(this.query.GetResoCountBubbleChartData(15, this.config.ResoFeedUrlAsia));
-					default:
-						return this.GetResponse(this.query.GetResoCountBubbleChartData(15, config.ResoFeedUrlNA));
-				}
-		}
+        [HttpGet]
+        public HttpResponseMessage Get(string region)
+        {
+            switch (region.ToLower().Trim())
+            {
+                case "eu":
+                    return this.GetResponse(this.query.GetResoCountBubbleChartData(60, this.config.ResoFeedUrlEU));
+                case "asia":
+                    return this.GetResponse(this.query.GetResoCountBubbleChartData(60, this.config.ResoFeedUrlAsia));
+                default:
+                    return this.GetResponse(this.query.GetResoCountBubbleChartData(60, config.ResoFeedUrlNA));
+            }
+        }
 
-		public HttpResponseMessage Get(string region, string metro)
-		{	
-			switch (region.ToLower().Trim())
-			{
-				case "eu":
-					return this.GetResponse(this.query.GetResoCountBubbleChartData(15, this.config.ResoFeedUrlEU, metro));
-				case "asia":
-					return this.GetResponse(this.query.GetResoCountBubbleChartData(15, this.config.ResoFeedUrlAsia, metro));
-				default:
-					return this.GetResponse(this.query.GetResoCountBubbleChartData(15, config.ResoFeedUrlNA, metro));
-			}
-		}
-	}
+        public HttpResponseMessage Get(string region, string metro)
+        {
+            switch (region.ToLower().Trim())
+            {
+                case "eu":
+                    return this.GetResponse(this.query.GetResoCountBubbleChartData(60, this.config.ResoFeedUrlEU, metro));
+                case "asia":
+                    return this.GetResponse(this.query.GetResoCountBubbleChartData(60, this.config.ResoFeedUrlAsia, metro));
+                default:
+                    return this.GetResponse(this.query.GetResoCountBubbleChartData(60, config.ResoFeedUrlNA, metro));
+            }
+        }
+    }
 }
--- OpenTableDataViz/Api/RadialChartController.cs
+++ OpenTableDataViz/Api/RadialChartController.cs
@@ -5,39 +5,52 @@ using System.Web;
 
 namespace OpenTableDataViz.Api
 {
-	using System.Net.Http;
-	using System.Web.Http;
+    using System.Net.Http;
+    using System.Web.Http;
 
-	using OpenTableDataViz.Services;
+    using OpenTableDataViz.Services;
 
-	public class RadialChartController : BaseApiController
-	{
-		private IBusinessQuery query;
+    public class RadialChartController : BaseApiController
+    {
+        private IBusinessQuery query;
 
-		private IAppConfiguration config;
+        private IAppConfiguration config;
 
-		public RadialChartController()
-		{
-		}
+        public RadialChartController()
+        {
+        }
 
-		public RadialChartController(IBusinessQuery query, IAppConfiguration configuration)
-		{
-			this.query = query;
-			this.config = configuration;
-		}
+        public RadialChartController(IBusinessQuery query, IAppConfiguration configuration)
+        {
+            this.query = query;
+            this.config = configuration;
+        }
 
-		[HttpGet]
-		public HttpResponseMessage Get(string region)
-		{
-			switch (region.ToLower().Trim())
-			{
-				case "eu":
-					return this.GetResponse(this.query.GetCuisineRadialChartData(15, this.config.ResoFeedUrlEU));
-				case "asia":
-					return this.GetResponse(this.query.GetCuisineRadialChartData(15, this.config.ResoFeedUrlAsia));
-				default:
-					return this.GetResponse(this.query.GetCuisineRadialChartData(15, config.ResoFeedUrlNA));
-			}
-		}
-	}
+        [HttpGet]
+        public HttpResponseMessage Get(string region)
+        {
+            switch (region.ToLower().Trim())
+            {
+                case "eu":
+                    return this.GetResponse(this.query.GetCuisineRadialChartData(60, this.config.ResoFeedUrlEU));
+                case "asia":
+                    return this.GetResponse(this.query.GetCuisineRadialChartData(60, this.config.ResoFeedUrlAsia));
+                default:
+                    return this.GetResponse(this.query.GetCuisineRadialChartData(60, config.ResoFeedUrlNA));
+            }
+        }
+
+        public HttpResponseMessage Get(string region, string metro)
+        {
+            switch (region.ToLower().Trim())
+            {
+                case "eu":
+                    return this.GetResponse(this.query.GetCuisineRadialChartData(60, this.config.ResoFeedUrlEU, metro));
+                case "asia":
+                    return this.GetResponse(this.query.GetCuisineRadialChartData(60, this.config.ResoFeedUrlAsia, metro));
+                default:
+                    return this.GetResponse(this.query.GetCuisineRadialChartData(60, config.ResoFeedUrlNA, metro));
+            }
+        }
+    }
 }
