a420466567a3a744cea197ab8e8f76ce81a7787d
Govind <githubgovin@gmail.com>
2013-04-02 13:08:36 -0700

    more updates...

--- OpenTableDataViz/App_Start/RouteConfig.cs
+++ OpenTableDataViz/App_Start/RouteConfig.cs
@@ -9,11 +9,29 @@ namespace OpenTableDataViz
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 			routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
+
+			routes.MapRoute(
+				"NARoute",
+				"na",
+				new { Controller = "Map", Action = "Live", id = UrlParameter.Optional });
+
+			routes.MapRoute(
+				"EURoute",
+				"eu",
+				new { Controller = "Map", Action = "Live", id = UrlParameter.Optional });
+
+			routes.MapRoute(
+				"AsiaRoute",
+				"jp",
+				new { Controller = "Map", Action = "Live", id = UrlParameter.Optional });
+
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
 				defaults: new { controller = "Map", action = "Live", id = UrlParameter.Optional }
 			);
+
+			
 		}
 	}
 }
---
+++ OpenTableDataViz/Filters/RedirectFilter.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Web.Routing;
+
+
+namespace OpenTableDataViz.Filters
+{
+	using System.Web.Mvc;
+
+	public class RedirectFilter : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			var request = filterContext.HttpContext.Request;
+
+			var urlList = new string[] { "/na", "/eu", "/jp" };
+
+			if (request.Url != null)
+			{
+				var absPath = request.Url.AbsolutePath.ToLower().TrimEnd('/');
+				var index = absPath.LastIndexOf("/");
+				var lastPartOfUrl = string.Empty;
+				if (index > -1)
+				{
+					lastPartOfUrl = absPath.Substring(index).ToLower();
+				}
+
+				if (urlList.Contains(lastPartOfUrl))
+				{
+
+					filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary()
+							{
+								{ "action", "Live" },
+								{ "controller", "Map" },
+							});
+				}
+			}
+		}
+	}
+}
